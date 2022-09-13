14e0db9fee248950a94cfbfe6a5d2c8a81e94dc4
Nikita Koshkarev <112462171+oldalpinist@users.noreply.github.com>
2022-09-13 20:52:27 +0800

    Update Quiz3.py

--- Quiz3.py
+++ Quiz3.py
@@ -1,24 +1,36 @@
-iPhone12 = [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]
-Xiaomi_Mi11 = [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]
-Samsung_Galaxy_21 = [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]
-print(iPhone12)
-print(Xiaomi_Mi11)
-print(Samsung_Galaxy_21)
-sumOfiPhone12 = sum(iPhone12)
-sumOfXiaomi_Mi11 = sum(Xiaomi_Mi11)
-sumOfSamsung_Galaxy_21 = sum(Samsung_Galaxy_21)
-print("Total sales for iPhone12:", sumOfiPhone12)
-print("Total sales for Xiaomi_Mi11:", sumOfXiaomi_Mi11)
-print("Total sales for sumOfSamsung_Galaxy_21:", sumOfSamsung_Galaxy_21)
-Average_value_iPhone12 = sumOfiPhone12/len(iPhone12)
-print ("Average value sales of the iPhone12:", Average_value_iPhone12)
-Average_value_Xiaomi_Mi11 = sumOfXiaomi_Mi11/len(Xiaomi_Mi11)
-print ("Average value sales of the Xiaomi_Mi11:", Average_value_Xiaomi_Mi11)
-Average_Samsung_Galaxy_21 = sumOfSamsung_Galaxy_21/len(Samsung_Galaxy_21)
-print ("Average value sales of the Samsung_Galaxy_21:", Average_Samsung_Galaxy_21)
-sumofallphones = sumOfiPhone12+sumOfXiaomi_Mi11+sumOfSamsung_Galaxy_21
-print ("Total sales for all phones:", sumofallphones)
-Average_value_allphones = Average_value_iPhone12+Average_value_Xiaomi_Mi11+Average_Samsung_Galaxy_21
-print ("Average value sales of the allphones:", Average_value_allphones)
+phone_sales = [
+  {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+  {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+  {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+]
 
+total_sum_iPhone12 = 0
+for sum_iPhone12 in phone_sales[0]["items_sold"][0:12]:
+    total_sum_iPhone12 += sum_iPhone12
+print("Total sales for iPhone12 is:", total_sum_iPhone12)
+total_sum_Xiaomi_Mi11 = 0
+
+for sum_Xiaomi_Mi11 in phone_sales[1]["items_sold"][0:12]:
+    total_sum_Xiaomi_Mi11 += sum_Xiaomi_Mi11
+print("Total sales for Xiaomi Mi11 is:", total_sum_Xiaomi_Mi11)
+
+totalsumSamsung_Galaxy_21 = 0
+for sum_Samsung_Galaxy_21 in phone_sales[2]["items_sold"][0:12]:
+    totalsumSamsung_Galaxy_21 += sum_Samsung_Galaxy_21
+print("Total sales for Samsung_Galaxy_21 is:", totalsumSamsung_Galaxy_21)
+
+avg_sales_iPhone12 = total_sum_iPhone12 / len(phone_sales[0]["items_sold"][0:12])
+print(f"average sales for iPhone12 is: {avg_sales_iPhone12}")
+
+avg_Xiaomi_Mi11 = total_sum_Xiaomi_Mi11 / len(phone_sales[1]["items_sold"][0:12])
+print(f"average sales for Xiaomi_Mi11 is: {avg_Xiaomi_Mi11}")
+
+avg_Samsung_Galaxy_21 = totalsumSamsung_Galaxy_21 / len(phone_sales[2]["items_sold"][0:12])
+print(f"average sales for Xiaomi_Mi11 is: {avg_Samsung_Galaxy_21}")
+# Посчитать и вывести суммарное количество продаж всех товаров
+sum_of_all_sales = total_sum_iPhone12+total_sum_Xiaomi_Mi11+totalsumSamsung_Galaxy_21
+print(f"sum sales for all phones is: {sum_of_all_sales}")
+#Посчитать и вывести среднее количество продаж всех товаров
+avg_sales_of_all_phones = avg_sales_iPhone12 + avg_Xiaomi_Mi11 + avg_Samsung_Galaxy_21
+print(f"average sales  for all phones is: {avg_sales_of_all_phones}")
 
